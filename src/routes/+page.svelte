580612c64cda9bb0972f8efd85ecc0c60000d6f0
Chea Virakbott <virakbott7@gmail.com>
2026-08-22 13:46:51 +0700

    save every click to database (#9)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Counter from '$lib/components/counter/Counter.svelte';
+
+	let { data }: { data: { count: number } } = $props();
 </script>
 
 <h1>Button Counter</h1>
-<Counter startingCount={0} />
+
+<Counter startingCount={data.count} />
